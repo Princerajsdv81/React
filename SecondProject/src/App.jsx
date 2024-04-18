b0d28f818c3c44586537c0c99632743de31f74d6
princerajsdv81 <princerajsdv81@gmail.com>
2024-04-18 17:22:50 +0530

    Second commit

--- SecondProject/src/App.jsx
+++ SecondProject/src/App.jsx
@@ -1,23 +1,12 @@
-import React, {Component} from 'react'
+import React from 'react'
+import Child from './Components/Props/Props1/Child'
+const App = () => {
+  return (
+    <div>
+     <Child userName="Prince"></Child>
 
-
-export default class App extends Component {
-    constructor(){
-        super()
-        this.state={
-           count:0
-        }
-    }
-    render(){
-        return (
-          <div  className='flex flex-col justify-center items-center h-[100vh] gap-3'>
-            <h1 className='font-[3rem]'>Count:{this.state.count}</h1>
-           <button className='bg-blue-400'  onClick={()=>{this.setState({count:this.state.count +1})}}>Increment+</button>
-           <button   onClick={()=>{this.state.count>0?this.setState({count:this.state.count-1}):alert('Negative number are not allowed')}}>Decrement-</button>
-           <button>Reset</button>
-          </div>
-        )
-
-    }
+    </div>
+  )
 }
 
+export default App
